5d4f016c8b354b7d8e74f59caf76a3d63d2eeb05
leprashimabuku <shimabuku@lepra.jp>
2018-11-15 22:28:19 +0900

    spring initializrからベースを作成

---
+++ src/main/java/com/github/jd1015/rindou/RindouApplication.java
@@ -0,0 +1,12 @@
+package com.github.jd1015.rindou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RindouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RindouApplication.class, args);
+	}
+}
