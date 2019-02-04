fbb207d35cb86290acf80d4c6af04715697dbd18
leprashimabuku <shimabuku@lepra.jp>
2019-02-04 23:03:45 +0900

    テストメールが送信できるところまで対応

--- src/main/java/com/github/jd1015/rindou/RindouApplication.java
+++ src/main/java/com/github/jd1015/rindou/RindouApplication.java
@@ -1,12 +1,32 @@
 package com.github.jd1015.rindou;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 @SpringBootApplication
 public class RindouApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RindouApplication.class, args);
-	}
+  public static void main(String[] args) {
+    try (ConfigurableApplicationContext ctx = SpringApplication.run(RindouApplication.class, args)) {
+      ctx.getBean(RindouApplication.class).sendMail();
+    }
+  }
+
+  @Autowired
+  private MailSender sender;
+
+  public void sendMail() {
+    SimpleMailMessage msg = new SimpleMailMessage();
+
+    msg.setFrom("test@example.com");
+    msg.setTo("test@example.com");
+    msg.setSubject("テストメール");
+    msg.setText("Spring Boot より本文送信");
+
+    this.sender.send(msg);
+  }
 }
